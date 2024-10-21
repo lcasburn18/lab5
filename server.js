f5fac6270669f61abac1864d8fc685adb56b00c8
g00405841 <g00405841@atu.ie>
2024-10-21 13:14:49 +0000

    Exercise 4

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/hello/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`Hello ${name}`);
+});
+
+app.get('/hello/:name/:lastname', (req, res) => {
+    const name = req.params.name;
+    const lastname = req.params.lastname;
+    res.send(`Hello ${name} ${lastname}`);
+});
+
 app.get('/', (req, res) => {
     res.send('Welcome to Data Representation & Querying');
 });
